db8e1c78c5565773459983e581efe25b66372e2b
NassimEloualydy <nassimaimadtahiri@gmail.com>
2024-03-16 13:56:28 +0100

    FLEX

--- App.js
+++ App.js
@@ -6,14 +6,41 @@ export default function App() {
   //get the Dimensions of screen
   // console.log(useWindowDimensions())
   return (
-    <View style={{
-      backgroundColor:"dodgerblue",
-      width:"100%",
-      height:Dimensions.get('window').width>Dimensions.get('window').height?"50%":"30%"
-    }}>
+    <View 
+    style={{
+      backgroundColor:"white",
+      // width:"100%",
+      // height:Dimensions.get('window').width>Dimensions.get('window').height?"50%":"30%"
+      flex:1
+    }}
+    >
       {/* <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" /> */}
-      
+      <View
+       style={{
+        backgroundColor:"dodgerblue",
+        // width:"100%",
+        // height:Dimensions.get('window').width>Dimensions.get('window').height?"50%":"30%"
+        flex:4
+      }}
+      />
+      <View
+       style={{
+        backgroundColor:"blue",
+        // width:"100%",
+        // height:Dimensions.get('window').width>Dimensions.get('window').height?"50%":"30%"
+        flex:1
+      }}
+      />
+      <View
+       style={{
+        backgroundColor:"orange",
+        // width:"100%",
+        // height:Dimensions.get('window').width>Dimensions.get('window').height?"50%":"30%"
+        flex:1
+      }}
+      />
+
     </View>
   );
 }
